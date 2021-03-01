1fab0fe3fad86bddf974378a5a130eb627fd3db1
adamjohnlea <imnogeekdptcom@me.com>
2021-02-28 19:57:34 -0700

    Initial Commit

---
+++ ListOfCountries/ListOfCountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListOfCountriesApp.swift
+//  ListOfCountries
+//
+//  Created by Adam Lea on 2/28/21.
+//
+
+import SwiftUI
+
+@main
+struct ListOfCountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
