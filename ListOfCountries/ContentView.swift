fbc692c2072ea259d9048f87dee225f4f6c888ce
adamjohnlea <imnogeekdptcom@me.com>
2021-03-11 12:36:07 -0700

    Add new features

--- ListOfCountries/ContentView.swift
+++ ListOfCountries/ContentView.swift
@@ -7,10 +7,69 @@
 
 import SwiftUI
 
+struct country:Identifiable {
+    let id = UUID()
+    let name: String
+    let flag: String
+}
+
 struct ContentView: View {
+    @State private var countries = [
+        country(name: "Australia", flag: "Australia"),
+        country(name: "Belgium", flag: "Belgium"),
+        country(name: "Canada", flag: "Canada"),
+        country(name: "Denmark", flag: "Denmark"),
+        country(name: "Finland", flag: "Finland"),
+        country(name: "Germany", flag: "Germany"),
+        country(name: "Japan", flag: "Japan"),
+        country(name: "United States of America", flag: "United-States")
+        
+    ]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach(countries) {country in
+                    NavigationLink(destination:
+                                    VStack{
+                                        Text(country.name).bold()
+                                        Image(country.flag)
+                                    }
+                    ){
+                        HStack {
+                            Image(country.flag)
+                            Text(country.name).bold()
+                        }
+                    }
+                }
+                .onDelete(perform: delete)
+                .onMove(perform: move)
+            }
+            .navigationTitle("List of Countries")
+            .toolbar{
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(action: addCountry,
+                    label: {Text("Add")}
+                    )
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    EditButton()
+                }
+            }
+        }
+    }
+    
+    func delete(at offsets: IndexSet) {
+        countries.remove(atOffsets: offsets)
+    }
+    
+    func move(from source: IndexSet, to destination: Int) {
+        countries.move(fromOffsets: source, toOffset: destination)
+    }
+    
+    func addCountry() {
+        countries.append(
+            country(name: "Norway", flag: "Norway")
+        )
     }
 }
 
